c317d3ca5527184311cb1fbb222f3f819c38391f
15771 <wjm7715@163.com>
2021-05-07 16:09:25 +0800

    测试异步代码

--- springboot-async/src/main/java/com/wjm/springbootasync/controller/AsyncTaskUse.java
+++ springboot-async/src/main/java/com/wjm/springbootasync/controller/AsyncTaskUse.java
@@ -1,9 +1,12 @@
 package com.wjm.springbootasync.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/api/v1.0/springbootAsync/asyncTaskUse")
@@ -15,5 +18,6 @@ public class AsyncTaskUse {
     @GetMapping("/startMysync")
     public void useMySyncTask() {
         treadTasks.startMyTreadTask();
+        System.out.println("controller: " + new Date());
     }
 }
--- springboot-async/src/main/java/com/wjm/springbootasync/controller/TreadTasks.java
+++ springboot-async/src/main/java/com/wjm/springbootasync/controller/TreadTasks.java
@@ -3,11 +3,19 @@ package com.wjm.springbootasync.controller;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
 @Component
 public class TreadTasks {
 
     @Async
     public void startMyTreadTask() {
         System.out.println("this is my async task");
+        try {
+            Thread.sleep(10000);
+            System.out.println("startMyTreadTask: " + new Date());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
