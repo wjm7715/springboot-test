5850e387b122aeaa71369d2c16ac4e4289a449b5
15771 <wjm7715@163.com>
2021-05-11 16:50:33 +0800

    springboot-mybatis模块代码 新增logback日志

--- springboot-mybatis/src/main/java/com/wjm/springbootmybatis/controller/UserController.java
+++ springboot-mybatis/src/main/java/com/wjm/springbootmybatis/controller/UserController.java
@@ -4,6 +4,8 @@ import com.wjm.springbootmybatis.entity.User;
 import com.wjm.springbootmybatis.service.UserServiceCsv;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +19,16 @@ import java.util.List;
 @Api(tags = "user")
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserServiceCsv userServiceCsv;
 
     @ApiOperation(value = "获取用户信息", notes = "获取所有用户信息", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @RequestMapping(value = "/getUserInfo", method = RequestMethod.GET)
     public List<User> getUserInfo() {
+        logger.info("Controller info: ");
+        logger.error("Controller error: ");
         return userServiceCsv.getUserInfo();
     }
 }
--- springboot-mybatis/src/main/java/com/wjm/springbootmybatis/impl/UserServiceCsvImpl.java
+++ springboot-mybatis/src/main/java/com/wjm/springbootmybatis/impl/UserServiceCsvImpl.java
@@ -3,6 +3,8 @@ package com.wjm.springbootmybatis.impl;
 import com.wjm.springbootmybatis.dao.UserMapper;
 import com.wjm.springbootmybatis.entity.User;
 import com.wjm.springbootmybatis.service.UserServiceCsv;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +13,15 @@ import java.util.List;
 @Service
 public class UserServiceCsvImpl implements UserServiceCsv{
 
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceCsvImpl.class);
+
     @Autowired
     private UserMapper userMapper;
 
     @Override
     public List<User> getUserInfo() {
+        logger.info("ServiceImpl info: ");
+        logger.error("ServiceImpl error: ");
         return userMapper.getUserInfo();
     }
 }
